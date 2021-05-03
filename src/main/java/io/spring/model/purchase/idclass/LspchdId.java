87de9e5a2214e46324a9dd5b5b6f1ebd952f910b
unknown <dnjswbf@gmail.com>
2021-05-03 13:08:52 +0900

    tmp

--- src/main/java/io/spring/model/purchase/idclass/LspchdId.java
+++ src/main/java/io/spring/model/purchase/idclass/LspchdId.java
@@ -1,7 +1,11 @@
 package io.spring.model.purchase.idclass;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LspchdId implements Serializable {
     //default serial version id, required for serializable classes.
     private static final long serialVersionUID = 1L;
