513e0229ad0bcab855bacfecb277620ac32eb2c0
unknown <dnjswbf@gmail.com>
2021-08-06 17:45:13 +0900

    EnableJpaRepositories annotation 추가

--- src/main/java/io/spring/TrdstApplication.java
+++ src/main/java/io/spring/TrdstApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,7 @@ import java.util.TimeZone;
 
 @Configuration
 @SpringBootApplication
+@EnableJpaRepositories
 @EntityScan("io.spring.model.*")
 public class TrdstApplication {
 
