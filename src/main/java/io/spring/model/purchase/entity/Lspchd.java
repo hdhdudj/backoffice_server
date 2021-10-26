c979ca7147b360d0c9b07100efd23482449ed8c6
pecan <pecan@trdst.com>
2021-10-26 14:17:45 +0900

    lspchd.purchaseItemAmt = tbOrderDetail.itemAmt -> tbOrderDetail.goodsPrice (왠지 아닌거같지만 일단 돌아가게 하기 위해..)

--- src/main/java/io/spring/model/purchase/entity/Lspchd.java
+++ src/main/java/io/spring/model/purchase/entity/Lspchd.java
@@ -150,7 +150,7 @@ public class Lspchd extends CommonProps implements Serializable {
         this.assortId = tbOrderDetail.getAssortId();
         this.itemId = tbOrderDetail.getItemId();
         this.purchaseQty = tbOrderDetail.getQty();
-        this.purchaseItemAmt = tbOrderDetail.getItemAmt();
+        this.purchaseItemAmt = tbOrderDetail.getGoodsPrice();
         this.purchaseUnitAmt = this.purchaseItemAmt * this.purchaseQty;
         this.itemGrade = StringFactory.getStrEleven(); // 11 하드코딩
         this.siteGb = StringFactory.getGbOne(); // 01 (고도몰) 하드코딩
