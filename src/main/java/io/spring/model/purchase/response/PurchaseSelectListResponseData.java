d20f5e446d16f335ff142c70d7263dbf2c2c1532
pecan <pecan@trdst.com>
2021-11-30 10:31:11 +0900

    주석 추가

--- src/main/java/io/spring/model/purchase/response/PurchaseSelectListResponseData.java
+++ src/main/java/io/spring/model/purchase/response/PurchaseSelectListResponseData.java
@@ -157,7 +157,7 @@ public class PurchaseSelectListResponseData {
 
         // 입고처리 화면 요소
         private String depositPlanId; // 입고예정번호
-        private Long availableQty; // 가능수량
+        private Long availableQty; // 가능수량(입고예정수량)
         private Long depositQty; // 입고수량
         private Float purchaseCost; // 발주금액
 		private String optionInfo;
