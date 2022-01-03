46730a44edef5f561122f25576b7234062e8bbe4
pecan <pecan@trdst.com>
2022-01-03 13:32:14 +0900

    goods detail request, response 수정:

--- src/main/java/io/spring/model/goods/response/GoodsInsertResponseData.java
+++ src/main/java/io/spring/model/goods/response/GoodsInsertResponseData.java
@@ -1,9 +1,12 @@
 package io.spring.model.goods.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.spring.infrastructure.custom.CustomLocalDateTimeDeSerializer;
 import io.spring.model.goods.entity.Ititmm;
 import io.spring.model.goods.request.GoodsInsertRequestData;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -27,15 +30,39 @@ public class GoodsInsertResponseData {
     }
 
     private String assortId;
-    private String assortNm;
     private String assortColor;
-    private String brandId;
-    private String origin;
-    private String manufactureNm;
-    private String assortModel;
-    private String taxGb;
-    // 21-12-21 추가
-    private String margin;
+    // 상품기본설정 화면
+    private String assortNm; // 상품명
+    private String assortModel; // 모델번호
+    private String taxGb; // 과세/면세
+    private String assortState; // 상품상태 : 진행중(01), 일시중지(02), 단품(03), 품절(04)
+    private String shortageYn; // 판매상태 : 진행중(01), 중지(02)
+    private String asLength; // 깊이
+    private String asHeight; // 높이
+    private String asWidth; // 너비
+    private String weight; // 무게
+    private String origin; // 원산지
+    private String brandId; // 브랜드 코드
+    private String dispCategoryId; // erp 카테고리 코드
+    private String manufactureNm; // 제조회사
+    private String localSale; // 판매가
+    @JsonDeserialize(using = CustomLocalDateTimeDeSerializer.class)
+    private LocalDateTime sellStaDt; // 판매기간 - 시작
+    @JsonDeserialize(using = CustomLocalDateTimeDeSerializer.class)
+    private LocalDateTime sellEndDt; // 판매기간 - 끝
+    private String deliPrice; // 매입가
+    private String localPrice; // 정가
+    private String margin; // 마진율
+
+    // 이미지 설정 화면
+
+    // 상품 가격 관리(MD팀) 화면
+    private String mdRrp; // RRP
+    private String mdYear; // 자료연도
+    private String mdTax; // TAX(자료)
+    private String mdVatrate; // 부가세율
+    private String mdDiscountRate; // 할인율
+    private String mdGoodsVatrate; // 상품마진율
 
     private List<GoodsInsertRequestData.Description> description;
     private List<Attributes> attributesList;
