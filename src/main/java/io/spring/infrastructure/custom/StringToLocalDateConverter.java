2ba07a87a32dda6cd8c886f19bcd8810e2620f65
pecan <pecan@trdst.com>
2022-01-12 17:50:07 +0900

    RequestBody 안의 LocalDate에 empty string이 들어오는 경우 처리해주는 converter의 주석 추가

---
+++ src/main/java/io/spring/infrastructure/custom/StringToLocalDateConverter.java
@@ -0,0 +1,26 @@
+package io.spring.infrastructure.custom;
+
+import org.springframework.boot.autoconfigure.web.format.WebConversionService;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * ReqeustBody LocalDate 요소에 ""가 오는 경우 null 리턴하는 컨버터
+ */
+@Component
+class StringToLocalDateConverter
+        implements Converter<String, LocalDate> {
+
+    @Override
+    public LocalDate convert(String source) {
+        if(source.trim().equals("")){
+            return null;
+        }
+        return LocalDate.parse(
+                source, DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
