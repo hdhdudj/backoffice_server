5488085337f70ad6a5bd9e706aa4e46b5c46a8ff
ryujaebong <rjb80@trdst.com>
2022-02-23 13:43:47 +0900

    itasrt itbrnd 엔티티수정

--- src/main/java/io/spring/model/goods/entity/Itasrt.java
+++ src/main/java/io/spring/model/goods/entity/Itasrt.java
@@ -15,8 +15,6 @@ import javax.persistence.Transient;
 
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 import org.hibernate.engine.spi.PersistentAttributeInterceptor;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -253,7 +251,6 @@ public class Itasrt extends CommonProps implements Serializable {
 	//// 다른 테이블과 엮으면 나오는 프로퍼티들
 	@JoinColumn(name = "brandId", referencedColumnName = "brandId", insertable = false, updatable = false, foreignKey = @javax.persistence.ForeignKey(name = "none"))
 	@JsonIgnore
-	@NotFound(action = NotFoundAction.IGNORE)
 	@ManyToOne(fetch = FetchType.LAZY) // Itbrnd 연관관계
 	private Itbrnd itbrnd;
 
