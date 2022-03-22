23424ec5990419c976095656d44e68b8baec5d79
pecan <pecan@gmail.com>
2022-03-22 10:06:43 +0900

    xml test

--- src/main/java/io/spring/controller/XmlTestController.java
+++ src/main/java/io/spring/controller/XmlTestController.java
@@ -6,7 +6,6 @@ import io.spring.model.stock.request.GoodsStockXml;
 import io.spring.service.stock.JpaStockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
