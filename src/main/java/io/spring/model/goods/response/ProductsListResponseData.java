00d537d7d749e7eea26da7d3049b52de653b90cb
ryujaebong <rjb80@trdst.com>
2022-04-07 10:50:45 +0900

    no message

--- src/main/java/io/spring/model/goods/response/ProductsListResponseData.java
+++ src/main/java/io/spring/model/goods/response/ProductsListResponseData.java
@@ -3,28 +3,12 @@ package io.spring.model.goods.response;
 import java.time.LocalDate;
 import java.util.List;
 
-<<<<<<< Updated upstream
-import io.spring.model.goods.entity.Products;
-import lombok.AccessLevel;
-=======
->>>>>>> Stashed changes
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-<<<<<<< Updated upstream
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ProductsListResponseData {
-	public ProductsListResponseData(String shortageYn, LocalDate regDtBegin, LocalDate regDtEnd, String assortId,
-			String assortNm) {
-		this.regDtBegin = regDtBegin;
-		this.regDtEnd = regDtEnd;
-		this.assortId = assortId;
-		this.shortageYn = shortageYn;
-		this.assortNm = assortNm;
-=======
 @NoArgsConstructor
 public class ProductsListResponseData {
 	public ProductsListResponseData(LocalDate regDtBegin, LocalDate regDtEnd, String saleYn, String displayYn,
@@ -38,17 +22,12 @@ public class ProductsListResponseData {
 		this.productNm = productNm;
 		this.masterId = masterId;
 		this.masterNm = masterNm;
->>>>>>> Stashed changes
+
 	}
 
 	private LocalDate regDtBegin;
 	private LocalDate regDtEnd;
-<<<<<<< Updated upstream
-	private String assortId;
-	private String shortageYn;
-	private String assortNm;
-	private List<Products> productsList;
-=======
+
 	private Long productId;
 
 	private String productNm;
@@ -57,6 +36,6 @@ public class ProductsListResponseData {
 	private Long masterId;
 	private String masterNm;
 	private List<ProductsResponseData> productsList;
->>>>>>> Stashed changes
+
 
 }
